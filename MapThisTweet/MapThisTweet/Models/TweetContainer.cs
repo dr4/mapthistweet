c9d0a51e6a557e4f870074f07651280cd881966d
Dzmitry Radchanka <dzmitry.radchanka@hotmail.com>
2015-11-15 01:37:49 +0800

    add user's name and avatar

--- MapThisTweet/MapThisTweet/Models/TweetContainer.cs
+++ MapThisTweet/MapThisTweet/Models/TweetContainer.cs
@@ -7,14 +7,25 @@ namespace MapThisTweet.Models
     {
         [JsonProperty(PropertyName = "date")]
         public DateTime CreatedAt { get; set; }
+
         [JsonProperty(PropertyName = "id")]
         public long Id { get; set; }
+
         [JsonProperty(PropertyName = "retweetCount")]
         public int RetweetCount { get; set; }
-        [JsonProperty(PropertyName = "userName")]
-        public string ScreenName { get; set; }
+
+        [JsonProperty(PropertyName = "user")]
+        public string User { get; set; }
+
+        [JsonProperty(PropertyName = "userFullName")]
+        public string UserFullName { get; set; }
+
+        [JsonProperty(PropertyName = "avatar")]
+        public string Avatar { get; set; }
+
         [JsonProperty(PropertyName = "text")]
         public string Text { get; set; }
+
         [JsonProperty(PropertyName = "cityId")]
         public long CityId { get; set; }
     }
