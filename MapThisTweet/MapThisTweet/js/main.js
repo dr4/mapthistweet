be8662299218d51101bbb243ce9736d9a68be7f7
Andrey Bodoev <shonoru@gmail.com>
2015-11-14 17:12:02 +0800

    WIP working map on local and remote

--- MapThisTweet/MapThisTweet/js/main.js
+++ MapThisTweet/MapThisTweet/js/main.js
@@ -1,10 +1,12 @@
-var isDev = location.host !== '' ? false : true;
-
 var isProdTest = false;
+/**
+ * TODO
+ * create script
+ * trigger
+ * local src
+ */
 
 $(function () {
-  console.log('test jquery');
-
   if (isDev) {
     $.getJSON('data/test.json', function (res) {
       console.log('test', res.test);
@@ -14,4 +16,19 @@ $(function () {
       console.log('tweets', res);
     })
   }
+
+  $('#map').on('init', function () {
+    console.log('map init');
+  })
 })
+
+var map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 8
+  });
+
+  $('#map').trigger('init');
+}
