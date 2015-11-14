a414ff5091cdb8b281c31bec6fbe74dd998ae806
Dzmitry Radchanka <dzmitry.radchanka@hotmail.com>
2015-11-14 20:28:03 +0800

    add simple tweet controller

--- MapThisTweet/MapThisTweet/Controllers/TweetsController.cs
+++ MapThisTweet/MapThisTweet/Controllers/TweetsController.cs
@@ -1,35 +1,97 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using Tweetinvi;
+using Tweetinvi.Core.Enum;
+using Tweetinvi.Core.Interfaces;
+using Tweetinvi.Core.Interfaces.Streaminvi;
 
 namespace MapThisTweet.Controllers
 {
     public class TweetsController : ApiController
     {
-        private static Models.Tweet[] tweets =
+        private static bool registered;
+        private static readonly ConcurrentQueue<TweetContainer> queue = new ConcurrentQueue<TweetContainer>();
+
+        public IEnumerable<TweetContainer> GetAllTweets()
         {
-            new Models.Tweet { Id = 1, Title = "First" },
-            new Models.Tweet { Id = 2, Title = "Second" },
-            new Models.Tweet { Id = 3, Title = "Third" }
-        };
-        public IEnumerable<Models.Tweet> GetAllTweets()
-        {
-            Auth.SetUserCredentials("CONSUMER_KEY", "CONSUMER_SECRET", "ACCESS_TOKEN", "ACCESS_TOKEN_SECRET");
-            return tweets;
+            if (!registered)
+            {
+                registered = true;
+                // TODO: add credentials
+                
+                Stream_SampleStreamExample();
+                return new TweetContainer[0];
+            }
+            else
+            {
+                stream.StopStream();
+
+                var list = new List<TweetContainer>();
+
+                TweetContainer tweet;
+                while(queue.TryDequeue(out tweet))
+                {
+                    list.Add(tweet);
+                }
+
+                return list;
+            }
         }
 
         public IHttpActionResult GetTweet(long id)
         {
-            var tweet = tweets.FirstOrDefault((t) => t.Id == id);
+            TweetContainer tweet = null;
             if (tweet == null)
             {
                 return NotFound();
             }
             return Ok(tweet);
+        }
+
+        private static IFilteredStream stream;
+
+        public static void Stream_SampleStreamExample()
+        {
+            stream = Stream.CreateFilteredStream();
+
+            stream.MatchingTweetReceived += (sender, args) =>
+            {
+                var tweet = args.Tweet;
+                if (!tweet.IsRetweet)
+                {
+                    var tweetContainer = new TweetContainer
+                    {
+                        CreatedAt = tweet.CreatedAt,
+                        ScreenName = tweet.CreatedBy.ScreenName,
+                        Id = tweet.Id,
+                        IsRetweet = tweet.IsRetweet,
+                        RetweetCount = tweet.RetweetCount,
+                        Text = tweet.Text
+                    };
+
+                    queue.Enqueue(tweetContainer);
+                }
+            };
+
+            stream.AddTweetLanguageFilter(Language.English);
+            stream.AddTrack("china");
+
+            stream.StartStreamMatchingAllConditionsAsync();
+        }
+
+        public class TweetContainer
+        {
+            public DateTime CreatedAt { get; set; }
+            public long Id { get; set; }
+            public bool IsRetweet { get; set; }
+            public int RetweetCount { get; set; }
+            public string ScreenName { get; set; }
+            public string Text { get; set; }
         }
     }
 }
