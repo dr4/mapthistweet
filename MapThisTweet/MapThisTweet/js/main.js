04c051644dd85ad787d69308e47ca27929fcf0bf
Andrey Bodoev <shonoru@gmail.com>
2015-11-14 23:28:09 +0800

    FIX

--- MapThisTweet/MapThisTweet/js/main.js
+++ MapThisTweet/MapThisTweet/js/main.js
@@ -18,7 +18,7 @@ var listOfCities = {}
 $(function () {
 
   function getCities(argument) {
-    var url = isDev ? DATAPATH + 'cities.json' : 'citys';
+    var url = isDev ? DATAPATH + 'cities.json' : 'api/citys';
 
     $.getJSON(url, function (data) {
       data.forEach(function (item) {
@@ -30,7 +30,7 @@ $(function () {
   }
 
   function getTweets() {
-    var url = isDev ? DATAPATH + 'tweets.json' : 'tweets';
+    var url = isDev ? DATAPATH + 'tweets.json' : 'api/tweets';
 
     $.getJSON(url, function (data) {
       stackOfTweets = data;
@@ -49,24 +49,27 @@ $(function () {
     if (stackOfTweets.length) {
       $(stackOfTweets).each(function (index, item) {
         var cityId = item.cityId;
-        var location = listOfCities[cityId]['location'];
-        var content = item.text;
+        var city = listOfCities[cityId];
 
-        var coords = new google.maps.LatLng(location.lat, location.lng);
+        if (cityId && city) {
+          var location = city['location'];
+          var content = item.text;
+          var coords = new google.maps.LatLng(location.lat, location.lng);
 
-        var infowindow = new google.maps.InfoWindow({
-          content: content,
-          maxWidth: 200
-        });
+          var infowindow = new google.maps.InfoWindow({
+            content: content,
+            maxWidth: 200
+          });
 
-        var marker = new google.maps.Marker({
-          position: coords,
-          map: map
-        });
+          var marker = new google.maps.Marker({
+            position: coords,
+            map: map
+          });
 
-        setTimeout(function () {
-          infowindow.open(map, marker);
-        }, 1000 + 1000 * index);
+          setTimeout(function () {
+            infowindow.open(map, marker);
+          }, 1000 + 1000 * index);
+        }
       })
 
     }
