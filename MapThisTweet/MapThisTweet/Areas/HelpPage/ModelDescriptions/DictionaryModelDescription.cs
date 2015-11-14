fc6935e89657fe7959422eb2bf753203b51e81eb
Dzmitry Radchanka <dzmitry.radchanka@hotmail.com>
2015-11-14 13:57:16 +0800

    Delete unused files

--- MapThisTweet/MapThisTweet/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace MapThisTweet.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- MapThisTweet/MapThisTweet/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace MapThisTweet.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
