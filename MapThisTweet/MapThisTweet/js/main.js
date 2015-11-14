2ea5d788ec945d5b2df681afe8bd4e4b81c84b80
Andrey Bodoev <shonoru@gmail.com>
2015-11-14 14:50:54 +0800

    WIP test local and prod api request
    * Add jquery
    * Update paths to relative

--- MapThisTweet/MapThisTweet/js/main.js
+++ MapThisTweet/MapThisTweet/js/main.js
@@ -1,3 +1,16 @@
-function init() {
-  console.log('init');
-}
+var isDev = true;
+
+$(function () {
+  console.log('test jquery');
+
+  if (isDev) {
+    $.getJSON('data/test.json', function (res) {
+      console.log('test', res.test);
+    })
+  } else {
+    $.getJSON('api/tweets', function (res) {
+      console.log('tweets', res);
+    })
+  }
+
+})
