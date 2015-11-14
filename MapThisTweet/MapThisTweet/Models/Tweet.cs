1e29fdf7bd6589879cd16116d6cb9f2045b8db46
Dzmitry Radchanka <dzmitry.radchanka@hotmail.com>
2015-11-14 14:20:59 +0800

    - api endpoint - add .gitattributes file

---
+++ MapThisTweet/MapThisTweet/Models/Tweet.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MapThisTweet.Models
+{
+    public class Tweet
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+    }
+}
