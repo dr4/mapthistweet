b72b1eed8fb5dba20131495445e04c0fdbdf8fe2
Andrey Bodoev <shonoru@gmail.com>
2015-11-14 20:00:33 +0800

    WIP sample tweets payload

--- MapThisTweet/MapThisTweet/js/main.js
+++ MapThisTweet/MapThisTweet/js/main.js
@@ -1,25 +1,53 @@
+var DATAPATH = 'data/';
+
 var isProdTest = false;
+
 /**
  * TODO
- * create script
- * trigger
- * local src
- * set center to china
+ * parse places
+ * parse payload
+ * show balloons
+ *
  */
 
 $(function () {
-  if (isDev) {
-    $.getJSON('data/test.json', function (res) {
-      console.log('test', res.test);
-    })
-  } else {
-    $.getJSON('api/tweets', function (res) {
-      console.log('tweets', res);
-    })
+  // example of response
+
+  // [
+  //   {
+  //     'id': '123', // place_id
+  //     'tweet': '', // html
+  //   },
+  //   ...
+  // ]
+
+  function getRequestUrl() {
+    var url = '';
+
+    if (isDev) {
+      url += DATAPATH + 'tweets.json'
+    } else {
+      url += 'tweets'
+    }
+
+    return url;
+  }
+
+  function requestData() {
+    var requestUrl = getRequestUrl();
+
+    $.getJSON(requestUrl, function (data) {
+      // show data with setTimeout
+
+    });
   }
 
   $('#map').on('init', function () {
-    console.log('map init');
+    requestData();
+  })
+
+  #('#map').on('show', function () {
+
   })
 })
 
