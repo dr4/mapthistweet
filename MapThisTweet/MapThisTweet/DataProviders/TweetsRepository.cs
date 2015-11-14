e78e1dcef43237b9cb4be2c3ac5cd434fc999b46
Dzmitry Radchanka <dzmitry.radchanka@hotmail.com>
2015-11-15 00:51:41 +0800

    read twitter credentials from appSettings

--- MapThisTweet/MapThisTweet/DataProviders/TweetsRepository.cs
+++ MapThisTweet/MapThisTweet/DataProviders/TweetsRepository.cs
@@ -2,6 +2,8 @@
 using MapThisTweet.Models;
 using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Configuration;
 using Tweetinvi;
 using Tweetinvi.Core.Enum;
 using Tweetinvi.Core.Interfaces.Streaminvi;
@@ -16,7 +18,7 @@ namespace MapThisTweet.DataProviders
 
         public static void Start()
         {
-            // Add AUTH
+            ConfigureTwitterCredentials();
 
             var random = new Random();
             int maxCityId = CitiesRepository.allCityIds.Length;
@@ -61,6 +63,17 @@ namespace MapThisTweet.DataProviders
         public static IEnumerable<TweetContainer> SelectAll()
         {
             return queue.ToArray();
+        }
+
+        private static void ConfigureTwitterCredentials()
+        {
+            NameValueCollection settings = ConfigurationManager.AppSettings;
+            string consumerKey = settings["consumerKey"];
+            string consumerSecret = settings["consumerSecret"];
+            string userAccessToken = settings["userAccessToken"];
+            string userAccessSecret = settings["userAccessSecret"];
+
+            Auth.SetUserCredentials(consumerKey, consumerSecret, userAccessToken, userAccessSecret);
         }
     }
 }
